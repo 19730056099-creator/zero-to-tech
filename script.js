88315ac2ad0265d6f6e3fe67b29bd6e519b01efd
MengZhouyi <19730056099@163.com>
2026-07-28 14:57:38 +0800

    第一次提交

---
+++ script.js
@@ -0,0 +1,6 @@
+function changeText() {
+        document.getElementById('msg').textContent = '你刚刚触发了一段 JavaScript。';
+      }
+function changeTitle() {
+document.getElementById('title').textContent = '标签已经改变';
+}
