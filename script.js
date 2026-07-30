2cb5b395c880ad78ddedfb8fd3f30cdf48a497b7
MengZhouyi <19730056099@163.com>
2026-07-30 17:26:44 +0800

    将项目中的代码替换成4.1所需的代码

--- script.js
+++
@@ -1,8 +0,0 @@
-"该文件下存放的是交互逻辑代码"
-
-function changeText() {
-        document.getElementById('msg').textContent = '你刚刚触发了一段 JavaScript。';
-      }
-function changeTitle() {
-document.getElementById('title').textContent = '标签已经改变';
-}
